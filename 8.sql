53e59cfa09642f4f737db95bec985f36c34ae142
OFFdevil <0808.20031@mail.ru>
2022-12-24 23:56:51 +0300

    update 8

--- 8.sql
+++ 8.sql
@@ -2,7 +2,7 @@ SET SEARCH_PATH = view_airport;
 
 --- Посчитать для каждого рейса, сколько раз его обновляли
 DROP VIEW IF EXISTS view_difficult_1;
-CREATE VIEW view_flights_history AS
+CREATE VIEW view_difficult_1 AS
     SELECT number as "number",
            count(*) as "count_updates"
     FROM airport_db.flights_history f
@@ -11,15 +11,25 @@ CREATE VIEW view_flights_history AS
     GROUP BY number
     ORDER BY count_updates;
 
----
-DROP VIEW IF EXISTS view_tickets_difficult;
-CREATE VIEW view_tickets AS
-    SELECT p.first_name || ' ' || p.last_name as "full_name",
-           t.number as "seat",
-           f.number as "flight_name",
-           f.start_time as "start_time"
-    FROM airport_db.tickets t
-        inner join airport_db.passengers p on t.passenger_id = p.passenger_id
-        inner join airport_db.flights f on t.flight_id = f.flight_id;
+-- ---
+-- DROP VIEW IF EXISTS view_difficult_2;
+-- CREATE VIEW view_difficult_2 AS
+--     SELECT p.first_name || ' ' || p.last_name as "full_name",
+--            t.number as "seat",
+--            f.number as "flight_name",
+--            f.start_time as "start_time"
+--     FROM airport_db.tickets t
+--         inner join airport_db.passengers p on t.passenger_id = p.passenger_id
+--         inner join airport_db.flights f on t.flight_id = f.flight_id;
 
----
+-- Найти всех сотрудников, которые работали не на свою компанию и вывести количество рейсов,
+-- которые они выполнили в компанией конкурентом
+DROP VIEW IF EXISTS view_difficult_3;
+CREATE VIEW view_difficult_3 AS
+    SELECT  (first_name || ' ' || last_name) as full_name,
+            e.employee_id,
+            count(f.flight_id)
+    FROM airport_db.employees as e
+        inner join airport_db.schedule_employees se on e.employee_id = se.employee_id
+        inner join airport_db.flights f on se.flight_id = f.flight_id and e.company_id <> f.company_id
+    GROUP BY full_name, e.employee_id;;
